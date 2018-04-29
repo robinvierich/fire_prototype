9f2025166882611fc401332844fe11d5bd46733d
Robin <robinvierich@gmail.com>
2018-04-29 16:52:18 -0400

    stub for FireSimulation

---
+++ Assets/Scripts/FireSimulation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSimulation {
+
+
+    public void StepSimulation(TileGrid tileGrid)
+    {
+
+    }
+
+}
